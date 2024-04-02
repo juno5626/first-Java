ec43124a3170bc03cdfeba30b792763a4ce4ea15
juno5626 <junono1@naver.com>
2024-04-02 20:04:44 +0900

    test

---
+++ CH01_Start/src/JeongHyunJun.java
@@ -0,0 +1,8 @@
+
+public class JeongHyunJun {
+	public static void main(String[] args) {
+		System.out.println("Name : JeongHyunJun");
+		System.out.println("University : Handong Global University");
+		System.out.println("Student Number : 22200698");
+	}
+}
